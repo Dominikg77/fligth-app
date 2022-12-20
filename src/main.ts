62d8db5e7e89a9eabe1d8dddf2618cb0b30a2f72
Dominik Graf <dominik.graf@2sic.com>
2022-12-20 10:07:19 +0100

    bug demo ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import './app/ts/demo'
+// import './app/ts/demo'
 
 if (environment.production) {
   enableProdMode();
